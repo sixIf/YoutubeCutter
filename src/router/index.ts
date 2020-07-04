2e7778bf593959b7d65b7dfad787a2c35469dd79
groCervo <samueltoko.nelson@gmail.com>
2020-07-04 09:24:57 +0200

    UP [index.ts] execute vue router in hash mode when app launched with electron

--- src/router/index.ts
+++ src/router/index.ts
@@ -79,7 +79,7 @@ const routes: Array<RouteConfig> = [
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: process.env.IS_ELECTRON ? 'hash' : 'history',
   base: process.env.BASE_URL,
   routes
 })
