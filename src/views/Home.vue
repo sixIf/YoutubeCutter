f0ab13626d08a670920200927abed817a54e5043
groCervo <samueltoko.nelson@gmail.com>
2020-08-01 16:29:24 +0200

    MINOR [Home.vue] change css in home cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,13 @@
     <v-container class="home-container fill-height" fluid>
       <v-row align="center" justify="center" no-gutters>
         <v-col cols="4" lg="4">
-          <v-card class="mx-auto my-12" max-width="374">
-            <v-card-title>Explore a specific Youtube channel</v-card-title>
+          <v-card elevation="15" min-height="200" class="mx-auto my-12" max-width="374">
+            <v-card-title>Explore a Youtube channel</v-card-title>
 
             <v-card-text>
               <v-row align="center" class="mx-0"></v-row>
 
-              <div>Display all the videos and playlists uploaded to a channel. Then you can choose what you want to download.</div>
+              <div>Display all the videos and playlists uploaded to a channel. Choose what you want to download.</div>
             </v-card-text>
 
             <v-divider class="mx-4"></v-divider>
@@ -19,14 +19,14 @@
             </v-card-actions>
           </v-card>
         </v-col>
-        <v-col cols="4" lg="4" offset="1" offset-md="0">
-          <v-card class="mx-auto my-12" max-width="374">
+        <v-col cols="4" lg="4" offset="1" offset-md="1">
+          <v-card elevation="15" min-height="200" class="mx-auto my-12" max-width="374">
             <v-card-title>Choose video by URLs</v-card-title>
 
             <v-card-text>
               <v-row align="center" class="mx-0"></v-row>
 
-              <div>You can find and download the videos of your choice simulteanously by copying and pasting their URLs.</div>
+              <div>Find and download the videos of your choice by copying and pasting their URLs.</div>
             </v-card-text>
 
             <v-divider class="mx-4"></v-divider>
